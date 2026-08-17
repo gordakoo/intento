5ed74a5056ec90a41420458e29f7a502fa45285d
gordako <auriculares5115@gmail.com>
2026-08-17 09:25:33 -0300

    Add files via upload

---
+++ proyecto.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-08-2026 a las 00:11:06
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `figuritas`
+--
+
+CREATE TABLE `figuritas` (
+  `numero` int(3) NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  `repetidas` int(3) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `estadio` varchar(40) NOT NULL,
+  `pais` varchar(30) NOT NULL,
+  `posicion` varchar(26) NOT NULL,
+  `club` varchar(50) NOT NULL,
+  `album` varchar(25) NOT NULL,
+  `año` int(4) NOT NULL,
+  `empresa` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
